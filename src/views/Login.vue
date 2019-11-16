d0f4e2dea024cf3d0a6d3ec36b156f073f43e1e4
cuixingdong <1440668914@qq.com>
2019-11-16 19:55:47 +0800

    登录功能

---
+++ src/views/Login.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="login">
+    <div class="project-name">Vue后台管理系统</div>
+
+    <el-form ref="form" :model="form" status-icon label-width="80px" :rules="rules">
+      <img src="../assets/avatar2.jpg" alt />
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
+      </el-form-item>
+      <el-form-item label="密  码" prop="password" >
+        <el-input v-model="form.password" placeholder="请输入密码" type="password"></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="login">登录</el-button>
+        <el-button @click="resetForm">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '用户名不能为空', trigger: ['change', 'blur'] },
+          { min: 3, max: 10, message: '用户名只能是3-10位', trigger: ['change', 'blur'] }
+        ],
+        password: [
+          { required: true, message: '密码不能为空', trigger: 'blur' },
+          { min: 6, max: 12, message: '密码只能是6-12位', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    resetForm () {
+      this.$refs.form.resetFields()
+      // console.log(this.$refs.form)
+    },
+    login () {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          console.log('该发送ajax请求了')
+          axios({
+            method: 'post',
+            url: 'http://localhost:8888/api/private/v1/login',
+            data: this.form
+          }).then(res => {
+            console.log(res.data)
+            if (res.data.meta.status === 200) {
+              this.$router.push('/home')
+            } else {
+              alert('登录失败了')
+            }
+          }).catch(error => {
+            console.log(error)
+          })
+        } else {
+          return false
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.login {
+  height: 100%;
+  width: 100%;
+  background: url("../assets/bg.jpg");
+    overflow: hidden;
+      .project-name{
+        width:100%;
+        font-size:38px;
+        height:100px;
+         color:#fff;
+           line-height: 200px;
+         text-align: center;
+
+      }
+  .el-form {
+    background-color: #fff;
+    width: 400px;
+    margin: 140px auto;
+    padding: 75px 40px 15px;
+    border-radius: 10px;
+    position: relative;
+    img {
+      position: absolute;
+      top: -75px;
+      left: 50%;
+      transform: translateX(-50%);
+      border-radius: 50%;
+      border: 10px solid #fff;
+      width: 120px;
+      height: 120px;
+      box-shadow: 0 1px 5px #ccc;
+    }
+  }
+}
+</style>
