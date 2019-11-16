c5fdba1c42e6e3c55e6138d5c8d86dfa903d1e8b
cuixingdong <1440668914@qq.com>
2019-11-16 15:30:26 +0800

    修改配置

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+// 作用：覆盖webpack的配置
+// module.exports = {
+//   // 覆盖webpack中的devServer
+//   devServer: {
+//     open: true,
+//     port: 9998
+//   }
+// }
+module.exports = {
+  // 覆盖webpack中的devServer
+  devServer: {
+    open: true,
+    port: 3000
+  }
+}
